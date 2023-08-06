0576f73681a1bee842a477b5afa8cd2d00e2ce7b
geran1ka <roman.khoruzhy@gmail.com>
2023-08-06 14:32:10 +0300

    add jquery ui init

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,7 @@
-// import {timerControl} from './timer/timerControl.js';
 import {renderBlog} from './module/renderBlog.js';
 import {getDataArticle} from './module/serviceApi.js';
 import {API_URL_POST} from './module/const.js';
 import {renderArticle} from './module/renderArticle.js';
-// import {renderPagination} from './module/renderPagination.js';
-// import {paginationController} from './module/paginationControl.js';
 import {timerInit} from './timer/timerInit.js';
 
 const init = async () => {
@@ -23,5 +20,4 @@ const init = async () => {
 };
 
 init();
-
 
---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener('resize', () => {
+  if (document.documentElement.clientWidth < 640) {
+    $('.footer__catalog').accordion({
+      active: true,
+      collapsible: true,
+      heightStyle: 'content',
+      icons: {
+        header: 'footer__accord-icon',
+        activeHeader: 'footer__accord-icon footer__accord-icon_active',
+      },
+    });
+
+    $('.footer__info').accordion({
+      active: true,
+      collapsible: true,
+      heightStyle: 'content',
+      icons: {
+        header: 'footer__accord-icon',
+        activeHeader: 'footer__accord-icon footer__accord-icon_active',
+      },
+    });
+  } else {
+    $('.footer__catalog').accordion('destroy');
+    $('.footer__info').accordion('destroy');
+  }
+});
+
+
