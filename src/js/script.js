4dfbfeef0e6b9dc4c46075b7b7910db7c5c28c80
geran1ka <roman.khoruzhy@gmail.com>
2023-08-12 09:01:26 +0300

    add header menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 import $ from 'jquery/dist/jquery.js';
 import 'jquery-ui/ui/widgets/accordion.js';
 
-const accordionInit = (elem) => {
+const accordionInit = (elem, selector) => {
   elem.accordion({
     active: true,
     collapsible: true,
     heightStyle: 'content',
     icons: {
-      header: 'footer__accord-icon',
-      activeHeader: 'footer__accord-icon footer__accord-icon_active',
+      header: `${selector}`,
+      activeHeader: `${selector} ${selector}_active`,
     },
   });
 };
@@ -16,10 +16,16 @@ const accordionInit = (elem) => {
 export const controllAccordeon = () => {
   const catalog = $('.footer__catalog');
   const info = $('.footer__info');
+  const menuCatalog = $('.menu__catalog');
+  const menuInfo = $('.menu__info');
+  const menuContacts = $('.menu__contacts');
   let accFlag = false;
   if (window.innerWidth <= 640) {
-    accordionInit(catalog);
-    accordionInit(info);
+    accordionInit(catalog, 'footer__accord-icon');
+    accordionInit(info, 'footer__accord-icon');
+    accordionInit(menuCatalog, 'menu__accord-icon');
+    accordionInit(menuInfo, 'menu__accord-icon');
+    accordionInit(menuContacts, 'menu__accord-icon');
     accFlag = true;
   }
 
@@ -27,12 +33,18 @@ export const controllAccordeon = () => {
     if (window.innerWidth <= 640) {
       accordionInit(catalog);
       accordionInit(info);
+      accordionInit(menuCatalog);
+      accordionInit(menuInfo);
+      accordionInit(menuContacts);
 
       accFlag = true;
     } else {
       if (accFlag) {
         catalog.accordion('destroy');
         info.accordion('destroy');
+        menuCatalog.accordion('destroy');
+        menuInfo.accordion('destroy');
+        menuContacts.accordion('destroy');
         accFlag = false;
       }
     }
