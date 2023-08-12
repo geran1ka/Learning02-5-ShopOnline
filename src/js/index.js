8985b2e72a8a658f1dfe49f7a80b0a463143f223
geran1ka <roman.khoruzhy@gmail.com>
2023-08-12 23:42:28 +0300

    add menu header

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,10 @@ import {getDataArticle} from './module/serviceApi.js';
 import {API_URL_POST} from './module/const.js';
 import {renderArticle} from './module/renderArticle.js';
 import {timerInit} from './module/timer/timerInit.js';
-import { controllAccordeon } from './script.js';
+import {controllAccordeon, menuControll} from './module/menu&accControl.js';
 
 const init = async () => {
+  menuControll();
   timerInit();
   controllAccordeon();
   const pageBlog = document.querySelector('.blog-page');
@@ -22,43 +23,3 @@ const init = async () => {
 };
 
 init();
-
-// const accordionInit = (elem) => {
-//   elem.accordion({
-//     active: true,
-//     collapsible: true,
-//     heightStyle: 'content',
-//     icons: {
-//       header: 'footer__accord-icon',
-//       activeHeader: 'footer__accord-icon footer__accord-icon_active',
-//     },
-//   });
-// };
-
-// export const controllAccordeon = () => {
-//   const catalog = $('.footer__catalog');
-//   const info = $('.footer__info');
-//   let accFlag = false;
-//   if (window.innerWidth <= 640) {
-//     accordionInit(catalog);
-//     accordionInit(info);
-//     accFlag = true;
-//   }
-
-//   window.addEventListener('resize', () => {
-//     if (window.innerWidth <= 640) {
-//       accordionInit(catalog);
-//       accordionInit(info);
-
-//       accFlag = true;
-//     } else {
-//       if (accFlag) {
-//         catalog.accordion('destroy');
-//         info.accordion('destroy');
-//         accFlag = false;
-//       }
-//     }
-//   });
-// };
-
-// controllAccordeon();
